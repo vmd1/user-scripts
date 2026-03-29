f747fac0318ff695358e911f2f57622804bfc36b
Vivaan <me@vmd1.dev>
2026-03-29 18:57:57 +0100

    Fix license typo in user script metadata

--- sme-bypass.user.js
+++ sme-bypass.user.js
@@ -5,7 +5,7 @@
 // @include       https://www.savemyexams.com/*
 // @include       https://www.savemyexams.com/
 // @version       16.0
-// @licence       MIT
+// @license       MIT
 // ==/UserScript==
 
 (function() {
